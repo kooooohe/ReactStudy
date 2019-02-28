e15eb072ff178688b68c9c7ae8631c5a6c56a0e9
近藤康平 <kondoukouhei@INV583noMacBook-Pro.local>
2019-02-28 13:37:19 +0900

    made hello component js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Hello from './hello';
 
 ReactDOM.render(
-  <div>Component</div>,
+  <Hello />,
   document.getElementById('root')
 );
